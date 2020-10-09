ea7ef2aa1ec0f784626375fd18524ea8b4644504
Sumithpd <https://github.com/sumithpdd/flutter_complete_guide.git>
2020-10-09 15:01:36 +0200

    validation

--- RoomBookingClient/src/app/admin/users/user-edit/user-edit.component.ts
+++ RoomBookingClient/src/app/admin/users/user-edit/user-edit.component.ts
@@ -1,37 +1,74 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { DataService } from 'src/app/data.service';
-import { User } from 'src/app/model/User';
+import {Component, Input, OnInit} from '@angular/core';
+import {User} from '../../../model/User';
+import {DataService} from '../../../data.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-user-edit',
   templateUrl: './user-edit.component.html',
-  styleUrls: ['./user-edit.component.css'],
+  styleUrls: ['./user-edit.component.css']
 })
 export class UserEditComponent implements OnInit {
+
   @Input()
   user: User;
 
   formUser: User;
+
   message: string;
+
   password: string;
-  constructor(private dataService: DataService, private router: Router) {}
+  password2: string;
+
+  nameIsValid = false;
+  passwordsAreValid = false;
+  passwordsMatch = false;
+
+  constructor(private dataService: DataService,
+              private router: Router) { }
 
   ngOnInit() {
     this.formUser = Object.assign({}, this.user);
+    this.checkIfNameIsValid();
+    this.checkIfPasswordsAreValid();
   }
+
   onSubmit() {
-    if(this.formUser.id==null){
-      this.dataService.addUser(this.formUser,this.password).subscribe((user) => {
-        this.router.navigate(['admin', 'users'], {
-          queryParams: { action: 'view', id: user.id },
-        });
-      });
-    }else{
-    this.dataService.updateUser(this.formUser).subscribe((user) => {
-      this.router.navigate(['admin', 'users'], {
-        queryParams: { action: 'view', id: user.id },
-      });
-    });}
+    if (this.formUser.id == null) {
+      this.dataService.addUser(this.formUser, this.password).subscribe(
+        (user) => {
+          this.router.navigate(['admin', 'users'], {queryParams: {action: 'view', id: user.id}});
+        }
+      );
+    } else {
+      this.dataService.updateUser(this.formUser).subscribe(
+        (user) => {
+          this.router.navigate(['admin', 'users'], {queryParams: {action: 'view', id: user.id}});
+        }
+      );
+    }
   }
+
+  checkIfNameIsValid() {
+    if (this.formUser.name) {
+      this.nameIsValid = this.formUser.name.trim().length > 0;
+    } else {
+      this.nameIsValid = false;
+    }
+  }
+
+  checkIfPasswordsAreValid() {
+    if (this.formUser.id != null) {
+      this.passwordsAreValid = true;
+      this.passwordsMatch = true;
+    } else {
+      this.passwordsMatch = this.password === this.password2;
+      if (this.password) {
+        this.passwordsAreValid = this.password.trim().length > 0;
+      } else {
+        this.passwordsAreValid = false;
+      }
+    }
+  }
+
 }
